84511145fd209fc23b915f47f91df28bd2772a74
YinYuKeJi_admin <326026637@qq.com>
2019-01-11 18:25:34 +0800

    gateway路由转发

---
+++ springcloud-gateway/src/main/java/com/springcloud/springcloudgateway/SpringcloudGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.springcloud.springcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+//eurekaclient启动注解
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringcloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGatewayApplication.class, args);
+    }
+
+}
